9e229b136ce5807571e906fd7069cf7da1ccceaa
Carlos <carlosrodriguezbotello.guadalupe@alumnado.fundacionloyola.net>
2025-06-11 18:55:06 +0200

    quitar config

--- config/configdb.php
+++
@@ -1,7 +0,0 @@
-<?php
-    //Datos de la Base de Datos
-    define("SERVIDOR",'esvirgua.com');
-    define("USUARIO",'user2daw_pruebassql');
-    define("PASSWORD",'{h%NNrE@0_5y');
-    define("BBDD",'user2daw_pruebassql');
-?>
